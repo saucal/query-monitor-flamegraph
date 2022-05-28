cde23db706e426958aec26da1ad5bdbdf231a72f
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-05-27 21:18:02 -0300

    Replace JS dependencies with external ones

--- query-monitor-flamegraph.php
+++ query-monitor-flamegraph.php
@@ -30,11 +30,10 @@ add_action( 'wp_enqueue_scripts', 'QM_Flamegraph\enqueue_scripts', 999 );
 add_action( 'admin_enqueue_scripts', 'QM_Flamegraph\enqueue_scripts', 999 );
 
 function enqueue_scripts() {
-	wp_register_script( 'qm-flamegraph-d3', plugins_url( 'js/d3-flame-graph/bower_components/d3/d3.js', __FILE__ ) );
-	wp_register_script( 'qm-flamegraph-d3-tip', plugins_url( 'js/d3-flame-graph/bower_components/d3-tip/index.js', __FILE__ ), array( 'qm-flamegraph-d3' ) );
-	wp_register_script( 'qm-flamegraph-lodash', plugins_url( 'js/d3-flame-graph/bower_components/lodash/lodash.js', __FILE__ ) );
-	wp_register_script( 'qm-flamegraph-d3-flamegraph', plugins_url( 'js/d3-flame-graph/src/d3.flameGraph.js', __FILE__ ), array( 'qm-flamegraph-d3', 'qm-flamegraph-d3-tip', 'qm-flamegraph-lodash' ) );
+	wp_register_script( 'qm-flamegraph-d3', 'https://d3js.org/d3.v7.js', array(), '7', true );
+	wp_register_script( 'qm-flamegraph-d3-flamegraph-tooltip', 'https://cdn.jsdelivr.net/npm/d3-flame-graph@4.1.3/dist/d3-flamegraph-tooltip.min.js', array( 'qm-flamegraph-d3' ), '4.1.3', true );
+	wp_register_script( 'qm-flamegraph-d3-flamegraph', 'https://cdn.jsdelivr.net/npm/d3-flame-graph@4.1.3/dist/d3-flamegraph.min.js', array( 'qm-flamegraph-d3', 'qm-flamegraph-d3-flamegraph-tooltip' ), '4.1.3', true );
 
 	wp_enqueue_script( 'qm-flamegraph-d3-flamegraph' );
-	wp_enqueue_style( 'qm-flamegraph-d3-flamegraph', plugins_url( 'js/d3-flame-graph/src/d3.flameGraph.css', __FILE__ ) );
+	wp_enqueue_style( 'qm-flamegraph-d3-flamegraph', 'https://cdn.jsdelivr.net/npm/d3-flame-graph@4.1.3/dist/d3-flamegraph.css', array(), '4.1.3' );
 }
